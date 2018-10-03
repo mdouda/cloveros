83de31b2f715f669bb6acf0e57128bdd88f6105c
TheNightman <joyod3@gmail.com>
2018-10-03 18:50:54 -0400

    Updates iso builder

--- liveCD/debian/make_iso.sh
+++ liveCD/debian/make_iso.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 ###############################################################
-### CloverOS liveCD creator
+### jOS liveCD creator
 ### make-iso.sh
-### https://cloveros.ga/ 
+### https://jOS.ga/ 
 ###
-### Makes a liveCD for CloverOS from latest debian XFCE iso
+### Makes a liveCD for jOS from latest debian XFCE iso
 ###
 ### License: DWTFYW
 ###############################################################
 
 # Colors, bb
-normal=$'\e[0m'; red=$'\e[31;01m'; green=$'\e[32;01m'; yellow=$'\e[33;01m'
+normal=$'\e[0m'; red=$'\e[31;01m'; green=$'\e[32;01m'; yellow=$'\e[33;01m';
 
 print_status() {
 	echo -n -e "${yellow}* ${green}${*}${normal}\n";
@@ -25,18 +25,50 @@ print_err() {
 	echo -n -e "${yellow}* ${red}${*}${normal} \n";
 }
 
+download_debian() {
+	print_status "What liveCD will you be building (xfce|kde|cinnamon|gnome|lxde|MATE)?"
+	read desktop_session
+
+	case "$desktop_session" in
+		xfce)
+			print_status "Downloading latest debian xfce livecd...";
+			wget "https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-9.5.0-amd64-xfce.iso" &>> make-iso.out
+			;;
+		kde)
+			print_status "Downloading latest debian kde livecd...";
+			wget "https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-9.5.0-amd64-kde.iso" &>> make-iso.out
+			;;
+		cinnamon)
+			print_status "Downloading latest debian cinnamon livecd...";
+			wget "https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-9.5.0-amd64-cinnamon.iso" &>> make-iso.out
+			;;
+		gnome)
+			print_status "Downloading latest debian gnome livecd...";
+			wget "https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-9.5.0-amd64-gnome.iso" &>> make-iso.out
+			;;
+		lxde)
+			print_status "Downloading latest debian lxde livecd...";
+			wget "https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-9.5.0-amd64-lxde.iso" &>> make-iso.out
+			;;
+		*)
+			print_status "Downloading latest debian mate livecd...";
+			wget "https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-9.5.0-amd64-mate.iso" &>> make-iso.out
+			;;
+	esac
+}
+
 # This script must be run as root!
 if [ "$(id -u)" != "0" ]; then
 	print_err "This script must be run as root!"
 	exit 1
 fi
 
-export SCRIPT_URL="https://cloveros.ga/s/installscript.sh"
+export SCRIPT_URL="https://github.com/TheNightmanCodeth/jOS/raw/jOS/installscript.sh"
 
 # Check for dependencies - 7z, mksquashfs, wget, and xorriso
 print_status "Checking for dependencies..."
 if [ ! -f /usr/bin/xorriso ] || [ ! -f /usr/bin/7z ] || [ ! -f /usr/bin/mksquashfs ] || [ ! -f /usr/bin/wget ]; then
-	print_neutral "In order to build a CloverOS liveCD, you must have 7-zip, wget and squashfs-tools installed. Install them now? (Y/n)"
+	print_neutral "In order to build a jOS liveCD, you must have 7-zip, wget and squashfs-tools installed. Install them now? (Y/n)"
 	read input
 
 	case "$input" in
@@ -45,7 +77,25 @@ if [ ! -f /usr/bin/xorriso ] || [ ! -f /usr/bin/7z ] || [ ! -f /usr/bin/mksquash
 			if [ ! -f /usr/bin/wget ]; then query="$query wget"; fi
 			if [ ! -f /usr/bin/mksquashfs ]; then query="$query squashfs-tools"; fi
 			if [ ! -f /usr/bin/7z ]; then query="$query p7zip"; fi
-			pacman -Syy $(echo "$query") &>> make-iso.out
+
+			if [ -f /etc/os-release ]; then
+				. /etc/os-release
+				OS=$NAME ## Will be 'Gentoo', 'Arch Linux', 'Debian GNU/Linux', 'Ubuntu'
+				case $OS in
+					"Gentoo")
+							emerge $(echo "$query ")  &>> make-iso.out
+							;;
+					"Arch Linux")
+							pacman -Syy $(echo "$query") &>> make-iso.out
+							;;
+					"Debian GNU/Linux")
+							apt install $(echo "$query") &>> make-iso.out
+							;;
+					"Ubuntu")
+							apt install $(echo "$query") &>> make-iso.out
+							;;
+				esac
+			fi			
 		;;
 		*)	
 			print_err "ERR: Missing dependencies. Exiting..."
@@ -56,46 +106,55 @@ fi
 
 # Download latest debian-live-x.x.x-amd64-xfce-desktop.iso from debian if not already
 if [ ! -e debian-live-*.iso ]; then
-	print_status "Downloading latest debian livecd...";
-	wget "https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-8.8.0-amd64-xfce-desktop.iso" &>> make-iso.out
+	download_debian
 else
-	print_status "Debian iso already exists. Continuing...";
+	print_status "Debian iso already exists. Are you building for a different DE?";
+	read -p "${yellow}  (y/N) -> ${red} " new_de
+	echo "${normal}"
+	case new_de in
+		[yY])
+			download_debian
+			;;
+		*)
+			print_status "Continuing..."
+			;;
+	esac
 fi
 
 export iso=$(ls "$(pwd)"/debian-live-* | tail -n1 | sed 's!.*/!!')
 
 # Extract iso contents
 print_status "Extracting iso contents..."
-7z x "$iso" -oCloverOS-DebianLive &>> make-iso.out
+7z x "$iso" -ojOS-DebianLive &>> make-iso.out
 
 # Extract the live filesystem
 print_status "Decompressing livecd filesystem..."
-cd CloverOS-DebianLive/live
+cd jOS-DebianLive/live
 unsquashfs filesystem.squashfs &>> make-iso.out
 cd ../..
 
-# LiveCD FS is now located at $(pwd)/CloverOS-DebianLive/live/squashfs-root/
+# LiveCD FS is now located at $(pwd)/jOS-DebianLive/live/squashfs-root/
 
 # Grab the installer and put it in place
-print_status "Downloading CloverOS install script and moving into place..."
-wget "$SCRIPT_URL" -O CloverOS-DebianLive/live/squashfs-root/usr/bin/CloverOS &>> make-iso.out
-echo '#!/bin/bash' | cat - CloverOS-DebianLive/live/squashfs-root/usr/bin/CloverOS > temp && mv temp CloverOS-DebianLive/live/squashfs-root/usr/bin/CloverOS
+print_status "Downloading jOS install script and moving into place..."
+wget "$SCRIPT_URL" -O jOS-DebianLive/live/squashfs-root/usr/bin/jOS &>> make-iso.out
+echo '#!/bin/bash' | cat - jOS-DebianLive/live/squashfs-root/usr/bin/jOS > temp && mv temp jOS-DebianLive/live/squashfs-root/usr/bin/jOS
 
 # Apply permissions
 print_status "Making script executable..."
-chmod +x CloverOS-DebianLive/live/squashfs-root/usr/bin/CloverOS &>> make-iso.out
+chmod +x jOS-DebianLive/live/squashfs-root/usr/bin/jOS &>> make-iso.out
 
 # Move over installer .desktop shortcut
 print_status "Installing script desktop entry..."
-cp install-cloveros.desktop CloverOS-DebianLive/live/squashfs-root/usr/share/applications/install-cloveros.desktop
+cp install-jOS.desktop jOS-DebianLive/live/squashfs-root/usr/share/applications/install-jOS.desktop
 
 ## TODO: Apply custom wallpaper /usr/share/wallpapers/Lines is default
-# rm CloverOS-DebianLive/live/squashfs-root/usr/share/wallpapers/Lines/contents/images/*.png
-# cp walpaper_dir/*.png CloverOS-DebianLive/live/squashfs-root/usr/share/wallpapers/Lines/contents/images/
+# rm jOS-DebianLive/live/squashfs-root/usr/share/wallpapers/Lines/contents/images/*.png
+# cp walpaper_dir/*.png jOS-DebianLive/live/squashfs-root/usr/share/wallpapers/Lines/contents/images/
 
 # Remove old live FS
 print_status "Removing old squashfs package..."
-cd CloverOS-DebianLive/live/
+cd jOS-DebianLive/live/
 rm -f filesystem.squashfs
 
 # Rebuild the live FS
@@ -112,19 +171,19 @@ cd ../..
 print_status "Building iso..."
 xorriso -as mkisofs -r -J \
        	-joliet-long -l -cache-inodes \
-       	-isohybrid-mbr /usr/lib/syslinux/bios/isohdpfx.bin \
+       	-isohybrid-mbr /usr/share/syslinux/isohdpfx.bin \
        	-partition_offset 16 -A "Debian Live" \
        	-b isolinux/isolinux.bin -c isolinux/boot.cat \
        	-no-emul-boot -boot-load-size 4 -boot-info-table  \
-	-o CloverOS-live-v0.01-xfce.iso CloverOS-DebianLive &>> make-iso.out
+	-o jOS-live-v0.01-xfce.iso jOS-DebianLive &>> make-iso.out
 
-print_neutral "Would you like to clean up (rm CloverOS-DebianLive)? (y/n)"
+print_neutral "Would you like to clean up \(rm jOS-DebianLive\)? \(y/n\)"
 read cleanup
 
 case "$cleanup" in
 	y|Y)
 	    print_status "Cleaning up..."
-	    rm -rf CloverOS-DebianLive/ &>> make-iso.out
+	    rm -rf jOS-DebianLive/ &>> make-iso.out
 esac
 
-print_status "All set! Iso is located at $(pwd)/CloverOS-live-v0.01-xfce.iso"
+print_status "All set! Iso is located at $(pwd)/jOS-live-v0.01-xfce.iso"
